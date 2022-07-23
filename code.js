133a6f9fa39f98ae3cc70c5190b08dc50771cab6
Andre-Grassi <76217972+Andre-Grassi@users.noreply.github.com>
2022-07-23 14:35:33 -0400

    Adding text when the user clicks the 'How to play' button

--- code.js
+++ code.js
@@ -3,11 +3,26 @@ console.log(howTo)
 
 howTo.addEventListener('click', showRules)
 
+let isActive = false
 function showRules() {
   let hideList = document.querySelectorAll(
     '#menu > div:first-child, #menu > hr'
   )
+
+  isActive = isActive == false ? true : false
+
+  // Hide elements to open space for text
   hideList.forEach(function (el) {
     el.toggleAttribute('hidden')
   })
+
+  // Get menu's section
+  let menu = document.getElementById('menu')
+  menuStyle = menu.style
+
+  if (isActive == true) menuStyle.backgroundImage = 'none' //disable background
+  else {
+    // Re-enable background
+    menuStyle.backgroundImage = 'url(Images/clover.png), url(Images/spade.png)'
+  }
 }
